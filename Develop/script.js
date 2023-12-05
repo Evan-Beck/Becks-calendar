e31f5ca8ec49391d7799f705bc32f81bd5c2c09f
unknown <ebeck703@gmail.com>
2023-12-04 19:36:08 -0500

    Third commit. Got all hours to work, save buttons save to local storage, sections are color coded and the calendar displays in the center.

--- Develop/script.js
+++ Develop/script.js
@@ -1,21 +1,26 @@
 $(function () {
-  // Add a listener for click events on the save button.
+  // Added an event listener, timeblock IDs, and  user input to be saved. The local storage saves user input by using the ID.
   $(".container-fluid").on("click", ".saveBtn", function () {
     var timeBlockId = $(this).parent().attr("id");
     var userInput = $(this).siblings(".description").val();
     localStorage.setItem(timeBlockId, userInput);
   });
 
-  // Dynamically generate time blocks for the entire day
+  // This function will generate time blocks for the day.
   function generateTimeBlocks() {
     var container = $(".container-fluid");
+    container.empty();
 
+// For loop sets the hours for the day to be generated. If the statement is true, the loop continues until 17 (5 PM).
     for (var hour = 9; hour <= 17; hour++) {
+      var period = hour >= 12 ? "PM" : "AM";
+      var displayHour = hour > 12 ? hour - 12 : hour;
+
       var timeBlock = $("<div>")
         .attr("id", "hour-" + hour)
         .addClass("row time-block")
         .html(`
-          <div class="col-2 col-md-1 hour text-center py-3">${hour > 12 ? hour - 12 + "PM" : hour + "AM"}</div>
+          <div class="col-2 col-md-1 hour text-center py-3">${displayHour}${period}</div>
           <textarea class="col-8 col-md-10 description" rows="3"></textarea>
           <button class="btn saveBtn col-2 col-md-1" aria-label="save">
             <i class="fas fa-save" aria-hidden="true"></i>
@@ -26,10 +31,10 @@ $(function () {
     }
   }
 
-  // Call the function to generate time blocks
+  // This calls the function to generate time blocks.
   generateTimeBlocks();
 
-  // Apply the past, present, or future class to each time block.
+  // This function applies the past, present, or future class to each time block.
   function updateHourClasses() {
     var currentHour = dayjs().hour();
 
@@ -46,10 +51,10 @@ $(function () {
     });
   }
 
-  // Call the function to update time block classes
+  //This function updates time block classes.
   updateHourClasses();
 
-  // Get any user input that was saved in localStorage.
+  // This function will grab any user input that was saved in localStorage.
   function loadSavedEvents() {
     $(".time-block").each(function () {
       var timeBlockId = $(this).attr("id");
@@ -58,9 +63,9 @@ $(function () {
     });
   }
 
-  // Call the function to load saved events
+  // This function calls to load saved events.
   loadSavedEvents();
 
-  // Display the current date in the header of the page.
+  // This will display the current date in the header of the page.
   $("#currentDay").text(dayjs().format("dddd, MMMM D, YYYY"));
 });
